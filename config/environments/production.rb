3f6034a87425e0646713119a162bf2cc25a5b6d9
Maneesh Sethi <maneesh@Maneeshs-MacBook-Pro.local>
2014-07-25 19:51:27 -0400

    Changing production.rb to precompile core.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,10 @@ Prelaunchr::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+config.assets.precompile += %w( public.js core.css  )
+
+
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
